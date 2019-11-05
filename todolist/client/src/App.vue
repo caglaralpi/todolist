d47c06dca6858aff04d52dfe87fb88b592a767c9
Çağlar Can SARIKAYA <caglarcansarikaya@hotmail.com>
2019-11-05 20:49:24 +0300

    python rest.api
    it is a todolist application with python for backend, vue.js for frontend

---
+++ todolist/client/src/App.vue
@@ -0,0 +1,13 @@
+
+<template>
+  <div class="container">
+    <button type="button" class="btn btn-primary">{{ msg }}</button>
+  </div>
+</template>
+
+
+<style>
+#app {
+  margin-left: 100px
+}
+</style>
